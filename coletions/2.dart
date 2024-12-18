75f9575ecf111f3eb0e2435926378d4c3f1a3fa6
Isabelly Lima <mslima5643@gmail.com>
2024-12-18 12:34:22 -0300

    up

--- coletions/2.dart
+++ coletions/2.dart
@@ -1,6 +1,23 @@
-void main(){
-  List<String> names=[
+void main() {
+  List<String> names = [
     'Isa', 'MARIA', 'Pedro', 'Luana'
   ];
-print(names);
+
+  
+  print(names);
+
+  // Outra maneira usando loop "for in"
+  for (String name in names.sublist(2, 3)) {
+    print(name.toUpperCase()); //recomendado
+  }
+
+  // Outra maneira usando forEach
+  names.forEach((name) {
+    print(name.toUpperCase());
+  });
+
+  // Outra maneira usando sublist com forEach
+  names.sublist(2).forEach((name) {
+    print(name.toUpperCase());
+  });
 }
