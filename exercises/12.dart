45a9ed1ad73ec3a3544cc1a6296f8f5301570e13
Isabelly Lima <mslima5643@gmail.com>
2025-01-16 11:09:56 -0300

    up

--- exercises/12.dart
+++ exercises/12.dart
@@ -1,1 +1,27 @@
 import 'dart:io';
+
+void main() {
+  Pessoa person = Pessoa();
+
+  print('Enter your name:');
+  person.name = stdin.readLineSync();
+
+  print('Enter your age:');
+  String? ageInput = stdin.readLineSync();
+  person.age = int.tryParse(ageInput ?? '');
+
+  print('Enter your height:');
+  String? heightInput = stdin.readLineSync();
+  person.height = double.tryParse(heightInput ?? '');
+
+  print('The entered details are:');
+  print('Name: ${person.name}');
+  print('Age: ${person.age ?? 'Invalid age'}');
+  print('Height: ${person.height ?? 'Invalid height'}');
+}
+
+class Pessoa {
+  String? name;
+  int? age;
+  double? height;
+}
